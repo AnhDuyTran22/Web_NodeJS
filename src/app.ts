e5f348b8b0aa6e1ce91ced36c813f249e67b54b3
AnhDuyTran.22 <anhduytrantb@gmail.com>
2025-09-13 13:03:14 +0700

    HTML FORM

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import 'dotenv/config'
+import webRoutes from "./routes/web";
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+
+
+// config routes
+webRoutes(app);
+
+
+// config static file: images/css/is
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+    console.log(`My app is running on port: ${PORT}`)
+    console.log("env port: ", process.env.PORT)
+})
